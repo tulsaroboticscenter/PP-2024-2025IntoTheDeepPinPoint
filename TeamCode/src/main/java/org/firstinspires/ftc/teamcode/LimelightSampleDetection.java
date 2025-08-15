c513badfd099d44850b22e7f2190db7aed27036b
Jaxroy07 <jaxonroy01@gmail.com>
2025-08-14 23:46:30 -0500

    Create LimelightSampleDetection.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightSampleDetection.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Limelight Test", group = "sensor")
+public class LimelightSampleDetection extends LinearOpMode {
+
+    double limelightMountAngleDegrees = -30;
+
+    double limelightHeightInches = 12.17;
+
+    double YAngle, XAngle;
+
+    @Override
+    public void runOpMode() {
+        Limelight3A limelight = hardwareMap.get(Limelight3A.class, "limelight");
+
+        limelight.pipelineSwitch(0);
+
+        limelight.start();
+
+        LLResult result = limelight.getLatestResult();
+
+        telemetry.addData(">", "Robot Ready. Press Play");
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            //Get x and y angles
+            if (result != null && result.isValid()) {
+                XAngle = result.getTx();
+                YAngle = result.getTy();
+
+                telemetry.addData("Target X", XAngle);
+                telemetry.addData("Target Y", YAngle);
+            } else {
+                telemetry.addData("limelight", "No Targets");
+            }
+
+            //Calculate angle to target from horizontal
+            double angleToTargetDegrees = limelightMountAngleDegrees + YAngle;
+            double angleToTargetRadians = angleToTargetDegrees * (3.14159/180);
+
+            //Calculate distance
+            double distanceToGoal = limelightHeightInches / Math.tan(angleToTargetRadians);
+            telemetry.addData("Distance to taget", distanceToGoal);
+
+            //Calculate Side to Side
+            double XOffset = Math.tan(XAngle) * distanceToGoal;
+            telemetry.addData("Displacement to the right", XOffset);
+
+            telemetry.update();
+        }
+   }
+
+}
